fdfdc9e880a6e56ae2a08762fe1bc6061691ecfe
Ash9944 <89572794+Ash9944@users.noreply.github.com>
2021-09-01 16:49:55 +0530

    Add files via upload

---
+++ JALAass4.js
@@ -0,0 +1,6 @@
+let person = {firstname:"Ashwajit" ,lastName: "Prasad"}
+console.log(person)
+console.log(person.firstname)
+console.log(person.lastName)
+console.log(person["firstname"])
+console.log(person["lastName"])
